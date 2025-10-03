f335ee323075cd57683587ae9696f666abb9f4a3
AJB <48493571+AngryJellyBean@users.noreply.github.com>
2025-10-03 00:55:35 -0500

    fixed missing import in schemas, added routes for user

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="ChunkLog API")
 
 app.include_router(users.router)
-app.include_router(foods.router)
-app.include_router(logs.router)
 
 @app.get("/")
 def root():
-    return {"message":"Okay!"}
+    return {"message":"CunkLog API running!"}
